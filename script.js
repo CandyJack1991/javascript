aada1df70a6987518738f627c15dcd2dc80576d2
candyjack1991 <candyjack1991@icloud.com>
2019-03-24 22:56:11 +0300

    Домашнее задание к уроку 1

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Задание №1 Перевисти градусы Цельсия в градусы Фаренгейта")
+var temperatura_celsius = prompt("Введите температуру в Цельсиях");
+console.log("Присваеваем переменой temperatura_celsius значение введеное с клавиатуры");
+var temperatura_farengate = (9/5) * temperatura_celsius + 32;
+console.log("Присваеваем пременной temperatura_farengate значение расчитаное по формуле ( (9/5) * temperatura_celsius + 32 )");
+alert("Температура по Фаренгейту" + temperatura_farengate );
+console.log("Температура по фаренгейту" + temperatura_farengate );
+console.log("Задние №2 Администратор Василий");
+var name = prompt ("Веддите ваше имя");
+console.log("Переменной Nama присваеваем значение с клавиатуры");
+var admin = name;
+console.log("Переменной admin присваеваем значение из переменной name");
+alert("Администратор" + admin);
+console.log("Вывод переменной admin");
+console.log("задание №3 Чему равно 1000 + '108'");
+alert(1000 + "108");
+console.log("Вывод результата");
